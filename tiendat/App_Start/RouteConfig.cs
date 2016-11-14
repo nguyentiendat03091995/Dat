889ca6272ecc72e88ffba09a2361f5eeefe57b6d
nguyentiendat <nguyentiendat03091995@gmail.com>
2016-11-14 15:48:49 +0700

    https://github.com/nguyentiendat03091995/Dat.git

--- tiendat/App_Start/RouteConfig.cs
+++ tiendat/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace tiendat
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "QLSanPham", action = "Themsp", id = UrlParameter.Optional }
             );
         }
     }
---
+++ tiendat/Controllers/QLSanPhamController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tiendat.Models;
+using System.IO;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+namespace tiendat.Controllers
+{
+    public class QLSanPhamController : Controller
+    {
+        //
+        // GET: /QLSanPham/
+        ibebizEntities1 db = new ibebizEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListSP()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Themsp()
+        {
+            
+            ViewBag.danhmuc = new SelectList(db.Brands.ToList().OrderBy(n=> n.Name), "ID", "danhmuc");
+            var danhmuc = from d in db.Brands
+                          select new { d.ID, d.Name };
+            var list = danhmuc.ToList().Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = c.ID.ToString(),
+                Selected = (c.ID == 1)
+            }).ToList();
+            ViewBag.danhmuc = list;
+            return View();
+        }
+        [HttpPost, ActionName("Themsp")]
+        public ActionResult Themsps(HttpPostedFileBase Photo, FormCollection cl)
+        {
+            ViewBag.danhmuc = new SelectList(db.Brands.ToList().OrderBy(n => n.Name), "ID", "danhmuc");
+                        Product pro = new Product();
+                        ProductL prol = new ProductL();
+
+                        var guid = Guid.NewGuid().ToString();
+                        //var nam = DateTime.Now.Year;
+                        //var thang = DateTime.Now.Month;
+                        //var ngay = DateTime.Now.Day;
+                        var pathFile = "/files/" + guid + "/images/";
+                        var fileName = Path.GetFileName(Photo.FileName);
+                        var path = Path.Combine(Server.MapPath(pathFile), fileName);
+                        bool exists = System.IO.Directory.Exists(Server.MapPath(pathFile));
+                        if (!exists)
+                        {
+                            System.IO.Directory.CreateDirectory(Server.MapPath(pathFile));
+                        }
+                        Photo.SaveAs(path);
+                        pro.DateFrom = DateTime.Parse(String.Format('{0:dd/MM/yyyy}', ));
+                        pro.DateTo = DateTime.Parse(Convert.ToString(pro.DateTo));
+                        pro.IsNew = true;
+                        db.Products.Add(pro);
+                        db.SaveChanges();
+            return View();
+        }
+	}
+}
